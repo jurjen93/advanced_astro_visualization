961309e8b554ba1dc2cd6fd2dae40560538db713
LucasPouw <lucaspouw@hotmail.nl>
2022-11-29 11:41:43 +0100

    Added a class for making scanning paths over the field and fixed a bug that occurred when crossing the prime meridian.

--- make_image.py
+++ make_image.py
@@ -1,13 +1,14 @@
-from poster.scripts.imaging import ImagingLofar
 import argparse
 import os
 
+from poster.scripts.imaging import ImagingLofar
+
 parser = argparse.ArgumentParser("Make cutout image from fits file.")
-parser.add_argument('-fi', '--fits', type=str, help='Fits file to use')
-parser.add_argument('-ra', '--right_ascension', type=float, help='RA in degrees')
-parser.add_argument('-dec', '--declination', type=float, help='DEC in degrees')
-parser.add_argument('-si', '--image_size', type=float, help='Image size in degrees (for squared region)')
-parser.add_argument('-ia', '--interactive', type=bool, default=True, help='Display image in interactive mode')
+parser.add_argument("-fi", "--fits", type=str, help="Fits file to use")
+parser.add_argument("-ra", "--right_ascension", type=float, help="RA in degrees")
+parser.add_argument("-dec", "--declination", type=float, help="DEC in degrees")
+parser.add_argument("-si", "--image_size", type=float, help="Image size in degrees (for squared region)")
+parser.add_argument("-ia", "--interactive", type=bool, default=True, help="Display image in interactive mode")
 args = parser.parse_args()
 
 if args.image_size:
@@ -15,15 +16,13 @@ if args.image_size:
 else:
     imsize = 0.4
 
-filename = '_'.join(args.fits.split('/')[-1].split('.')[0:-1]+[str(args.right_ascension), str(args.declination)])
+filename = "_".join(args.fits.split("/")[-1].split(".")[0:-1] + [str(args.right_ascension), str(args.declination)])
 
-Image = ImagingLofar(fits_file=args.fits, image_directory='cutouts', verbose=False)
-Image.image_cutout(image_name=filename+'.png',
-              dpi=100, pos=(args.right_ascension, args.declination), imsize=imsize)
-print(f'Made {filename}.png')
-Image.make_fits(pos=(args.right_ascension, args.declination), imsize=0.4,
-                filename=filename+'.fits')
-print(f'Made {filename}.fits')
+Image = ImagingLofar(fits_file=args.fits, image_directory="cutouts", verbose=False)
+Image.image_cutout(image_name=filename + ".png", dpi=100, pos=(args.right_ascension, args.declination), imsize=imsize)
+print(f"Made {filename}.png")
+Image.make_fits(pos=(args.right_ascension, args.declination), imsize=0.4, filename=filename + ".fits")
+print(f"Made {filename}.fits")
 
 if args.interactive:
-    os.system(f'python make_interactive.py -fi cutouts/{filename}.fits')
+    os.system(f"python make_interactive.py -fi cutouts/{filename}.fits")
